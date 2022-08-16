ba939cd3b98226790de6ad2d455c25cdd00ebf7f
benjirees <benreeseng@gmail.com>
2022-08-16 22:27:33 +0100

    Updates

---
+++ precache-manifest.380e2d9aa5ceff2dec4316830cb63941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e702f9477e4febddc0e",
+    "url": "/bimdashboard/static/css/main.b3c8b34f.chunk.css"
+  },
+  {
+    "revision": "0e702f9477e4febddc0e",
+    "url": "/bimdashboard/static/js/main.0e702f94.chunk.js"
+  },
+  {
+    "revision": "99fdde5dd8da7e221122",
+    "url": "/bimdashboard/static/js/1.99fdde5d.chunk.js"
+  },
+  {
+    "revision": "25ee3e93329c42bb56c0",
+    "url": "/bimdashboard/static/js/runtime~main.25ee3e93.js"
+  },
+  {
+    "revision": "1f64ac2718db47293b08d9bb1eca8d8f",
+    "url": "/bimdashboard/static/media/allplan-logo.1f64ac27.svg"
+  },
+  {
+    "revision": "945364e3cd5baccad316b36f530c9a90",
+    "url": "/bimdashboard/index.html"
+  }
+];
