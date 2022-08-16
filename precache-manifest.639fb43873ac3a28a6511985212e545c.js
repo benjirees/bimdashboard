abf622f83c1cd04f405857ff7bdb1482971a7627
benjirees <benreeseng@gmail.com>
2022-08-16 16:44:43 +0100

    Updates

---
+++ precache-manifest.639fb43873ac3a28a6511985212e545c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e160c3709c4ae3d3150",
+    "url": "/bimdashboard/static/css/main.63bccdc8.chunk.css"
+  },
+  {
+    "revision": "3e160c3709c4ae3d3150",
+    "url": "/bimdashboard/static/js/main.3e160c37.chunk.js"
+  },
+  {
+    "revision": "7b78dcedf69fb1f2a6c2",
+    "url": "/bimdashboard/static/js/1.7b78dced.chunk.js"
+  },
+  {
+    "revision": "25ee3e93329c42bb56c0",
+    "url": "/bimdashboard/static/js/runtime~main.25ee3e93.js"
+  },
+  {
+    "revision": "1f64ac2718db47293b08d9bb1eca8d8f",
+    "url": "/bimdashboard/static/media/allplan-logo.1f64ac27.svg"
+  },
+  {
+    "revision": "9e5feea9841e7285ce07310d11cd1943",
+    "url": "/bimdashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bimdashboard/precache-manifest.a40ea8ec12765962b803934df2d979e9.js"
+  "/bimdashboard/precache-manifest.639fb43873ac3a28a6511985212e545c.js"
 );
 
 workbox.clientsClaim();
